3bb6e47e3744579a71997505120798e5154f5e0f
Radosław Warzocha <rwar@softserveinc.com>
2017-03-12 16:06:45 +0100

    Change render_to_response to render

--- code/channels_chat/channels_chat/views.py
+++ code/channels_chat/channels_chat/views.py
@@ -1,13 +1,13 @@
 # -*- encoding: utf-8 -*-
 
 from django.http import JsonResponse
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 
 from .models import Message
 
 
 def home(request):
-    return render_to_response('channels_chat/index.html')
+    return render('channels_chat/index.html')
 
 
 def messages(request):
