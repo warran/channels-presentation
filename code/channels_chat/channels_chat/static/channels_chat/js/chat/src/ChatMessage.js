0409109db71da09ab2b449e322a56789b6ab8427
Radosław Warzocha <rwar@softserveinc.com>
2017-03-28 21:33:58 +0200

    Msg list styling

--- code/channels_chat/channels_chat/static/channels_chat/js/chat/src/ChatMessage.js
+++ code/channels_chat/channels_chat/static/channels_chat/js/chat/src/ChatMessage.js
@@ -4,14 +4,23 @@
 
 import React from 'react';
 
+const mapStateToProps = (state) => {
+    return {
+        currentUser: state.currentUser,
+    };
+};
+
 class ChatMessage extends React.Component
 {
     render() {
+        const msg_class = "msg" +
+            (this.props.currentUser === this.props.user ? "user_msg" : "");
+
         return (
-            <div className="msg">
+            <div className={msg_class}>
                 <div className="msg_header">
-                    <span>{this.props.user}</span>
-                    <span>{this.props.time}</span>
+                    <span className="msg_username">{this.props.user}</span>
+                    <span className="msg_time">{this.props.time}</span>
                 </div>
                 <div className="msg_content">
                     {this.props.message}
